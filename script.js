e9de13fcf334873c56cb9f30e5bba88484465ec3
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-01 22:14:17 +0300

    выполнил задание 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const a = "title";
+const b = "screens";
+const c = "screenPrice";
+const d = "rollback";
+const e = "fullPrice";
+const i = "adaptive";
+
+alert("Hey dude!");
+
+console.log("Hello dude!");
+
