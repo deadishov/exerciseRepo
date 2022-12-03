7dc369936eb8485fc0d4fc0f762f57977efd5ed3
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-03 14:58:32 +0300

    Вывел в консоль типы данных некоторых переменных

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'Just project';
+console.log(typeof title);
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 50;
+let rollback = 35;
+let fullPrice = 85;
+console.log(typeof fullPrice);
+let adaptive = true;
+console.log(typeof adaptive);
