a63311ecb0dff030b6a5565db01e415cee02932a
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-03 18:42:23 +0300

    Вывел значение длины строки screens в консоль

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let title = 'Just project';
 console.log(typeof title);
 let screens = 'Простые, Сложные, Интерактивные';
+console.log(screens.length);
 let screenPrice = 50;
 let rollback = 35;
 let fullPrice = 85;
