dc189a43e7c98c5f04d462e40785dacdc5584f3c
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-03 17:39:21 +0300

    Вывел в консоль длину строки screens

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let title = 'Just project';
 console.log(typeof title);
 let screens = 'Простые, Сложные, Интерактивные';
+console.log(screens.length);
 let screenPrice = 50;
+console.log('Стоимость верстки экранов ' + screenPrice + ' долларов');
 let rollback = 35;
 let fullPrice = 85;
 console.log(typeof fullPrice);
 let adaptive = true;
 console.log(typeof adaptive);
+
