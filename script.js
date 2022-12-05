bd810aa5943fbbb863e3948db304e8d9a65bb294
Muhammad Dedishov <moxmad99@mail.ru>
2022-12-05 23:29:48 +0300

    внес правки

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let title = 'Just project';
-console.log(typeof title);
-let screens = 'Простые, Сложные, Интерактивные'; { }
-console.log(screens.length);
-console.log(screens.toLowerCase());
-console.log(screens.split(','));
+let screens = 'Простые, Сложные, Интерактивные';
 let screenPrice = 50;
-console.log('Стоимость верстки экранов ' + screenPrice + ' долларов');
 let rollback = 35;
 let fullPrice = 85;
+let adaptive = true;
+
+
+console.log(typeof title);
+console.log(screens.length);
+console.log(screens.toLowerCase(), screens.split(', '));
+console.log('Стоимость верстки экранов ' + screenPrice + ' долларов');
+console.log(typeof adaptive);
 console.log('Стоимость разработки сайта ' + fullPrice + ' долларов');
 console.log(typeof fullPrice);
-let adaptive = true;
-console.log(typeof adaptive);
-
 console.log(fullPrice * rollback / 100);
